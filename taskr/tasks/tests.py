0f7886cf8acfeefdb29c5656b00e93d64a53847e
gitsomecode <sjbitcode@gmail.com>
2016-09-10 18:36:11 -0400

    Write unit test for GET task list.

--- taskr/tasks/tests.py
+++ taskr/tasks/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Task
+
+User = get_user_model()
+
+
+class TasksTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'testuser',
+            'testuser@email.com',
+            'testuser',
+            is_staff=True,
+            is_superuser=True
+        )
+
+    def test_get_tasks(self):
+        url = reverse('task-list')
+        token = self.user.auth_token
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
